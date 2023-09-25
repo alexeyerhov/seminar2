9335879ccc09e41f6e096b44da5030b5d2acae57
Alexey <>
2023-09-25 14:57:10 +0300

    Decided 1st program

---
+++ threeprograms/program1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+           void FindSecondOfNumber(int num){
+            if ( num > 99 && num < 1000 ){
+                int figure2 = num/10%10;
+                Console.WriteLine($"Second figure of number is {figure2}.");
+            }
+            else if( num<-99 && num > -1000 ) {
+                num*=-1;
+                int figure2 = num/10%10;
+                Console.WriteLine($"Second figure of number is {figure2}.");
+            }
+            else {
+                Console.WriteLine("Error, enter correct number!");
+            }
+           }
+
+
+           Console.WriteLine("Please enter three-digit number:");
+           int number = Convert.ToInt32(Console.ReadLine());
+           FindSecondOfNumber(number);
+        }
+    }
+}
