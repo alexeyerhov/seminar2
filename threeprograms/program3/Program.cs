8965d7a1fd17ba9f951d82d1984396e59c53a57f
Alexey <>
2023-09-25 15:19:57 +0300

    Decited third program, finally!

---
+++ threeprograms/program3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace program3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+            void checkForWeekend(int numOfDay){
+                if (numOfDay==6 || numOfDay==7){
+                    Console.WriteLine("Yes, your day is a weekend!");
+                }
+                else {
+                    Console.WriteLine("No, it`s not a weekend!");
+                }
+
+            }
+
+
+            Console.WriteLine("Enter the number of day:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            checkForWeekend(number);
+        }
+    }
+}
